a4091db4584f03c2d30a4885c46b46a0ace48333
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-08-12 15:10:20 +0100

    chore: optimize label checks

--- src/handlers/checks/label.ts
+++ src/handlers/checks/label.ts
@@ -2,27 +2,29 @@ import { Context } from "../../types";
 import { addCommentToIssue } from "../../shared/comment";
 import { fetchTopLanguages } from "../fetching/fetch-language-stats";
 
-export async function handleLabelChecks(context: Context, token: string, labelFilters: string[], xpTiers: Record<string, number>, user: string) {
+export async function handleLabelChecks(context: Context, token: string, configLabelFilters: string[], xpTiers: Record<string, number>, user: string) {
   const { logger } = context;
   const langs = await fetchTopLanguages(user, token);
-  const userLanguages = new Set(
+  const normalizedUserLanguages = new Set(
     Object.values(langs).map((lang) => {
       return {
         name: lang.name.toLowerCase(),
-        percentage: lang.percentage,
+        percentage: lang.percentage || 0,
       };
     })
   );
   const issueLabels = context.payload.issue.labels.map((label) => {
     const rankCaptureRegex = /\(([^)]+)\)/;
-    return {
-      name: label.name.split(":")[0].toLowerCase(),
-      tier: rankCaptureRegex.exec(label.name)?.[1]?.toLowerCase() || "n/a",
-    };
-  });
+    if (label.name.includes(":")) {
+      return {
+        name: label.name.split(":")[0].toLowerCase(),
+        tier: rankCaptureRegex.exec(label.name)?.[1]?.toLowerCase() || "n/a",
+      };
+    }
+  }).filter((label) => label !== undefined);
 
   // find the label we wish to use as our guard
-  const labelFilters_ = issueLabels.filter((label) => labelFilters.some((filter) => filter.toLowerCase() === label.name));
+  const labelFilters_ = issueLabels.filter((label) => configLabelFilters.some((filter) => filter.toLowerCase() === label?.name));
 
   if (labelFilters_.length === 0) {
     logger.info(`No label guard found for ${user}`);
@@ -32,7 +34,7 @@ export async function handleLabelChecks(context: Context, token: string, labelFi
   const msg: string[] = [];
 
   const { highestTieredDuplicates, msg: duplicateMsgs } = await findAndRemoveDuplicateFilters(labelFilters_, xpTiers);
-  const checked = await checkLabelGuards(context, userLanguages, highestTieredDuplicates, xpTiers, user);
+  const checked = await checkLabelGuards(context, normalizedUserLanguages, highestTieredDuplicates, xpTiers, user);
 
   if (duplicateMsgs.length > 0 || checked.msg.length > 0) {
     msg.push(duplicateMsgs.join(""));
@@ -54,22 +56,23 @@ async function findAndRemoveDuplicateFilters(labelFilters: { name: string; tier:
   let count = 0;
   labelFilters.forEach((label) => {
     const currentTierValue = xpTiers[label.tier];
-    const existingEntry = highestTierMap[label.name];
+    const name = label.name.toLowerCase();
+    const existingEntry = highestTierMap[name];
 
     if (!existingEntry) {
-      highestTierMap[label.name] = label;
+      highestTierMap[name] = label;
       return;
     }
 
     const existingTierValue = xpTiers[existingEntry.tier];
 
     if (currentTierValue > existingTierValue) {
-      highestTierMap[label.name] = label;
+      highestTierMap[name] = label;
       count++;
-      msg.push(`\n- ${label.name} (${label.tier})`);
+      msg.push(`\n- ${name} (${label.tier})`);
     } else if (currentTierValue <= existingTierValue) {
       count++;
-      msg.push(`\n- ${label.name} (${existingEntry.tier})`);
+      msg.push(`\n- ${name} (${existingEntry.tier})`);
     }
   });
 
@@ -88,34 +91,26 @@ async function checkLabelGuards(
 ) {
   const { logger } = context;
 
-  const normalizedLabelFilters = labelFilters.map(({ name }) => name.toLowerCase());
-  const normalizedUserLanguages = new Set(Array.from(userLanguages).map(({ name }) => name));
+  const labelTierMap = new Map(labelFilters.map(({ name, tier }) => [name, tier]));
+  const userLanguageMap = new Map(Array.from(userLanguages).map(({ name, percentage }) => [name, percentage]));
 
   let hasPassed = true;
-
   const msg: string[] = [];
 
-  for (const labelFilter of normalizedLabelFilters) {
-    if (!normalizedUserLanguages.has(labelFilter)) {
+  for (const labelFilter of labelFilters.map(({ name }) => name)) {
+    const userLangPercentage = userLanguageMap.get(labelFilter);
+
+    if (!userLangPercentage) {
       const logMessage = logger.error(`${user} does not have the required language for: ${labelFilter}`);
       msg.push(`\n! ${logMessage?.logMessage.raw}`);
       hasPassed = false;
       continue;
     }
 
-    const userLang = Array.from(userLanguages).find(({ name }) => name === labelFilter);
-    if (!userLang) {
-      const logMessage = logger.error(`${user} failed to pass the required language guard for ${labelFilter}`);
-      hasPassed = false;
-      msg.push(`\n! ${logMessage?.logMessage.raw}`);
-      continue;
-    }
-
-    const tier = labelFilters.find(({ name }) => name === labelFilter)?.tier || "n/a";
+    const tier = labelTierMap.get(labelFilter) || "n/a";
 
     if (tier === "n/a") {
       logger.info("No label guard found, skipping...");
-      // TODO: could have more robust handling depending on ranks etc but needs thought out
       continue;
     }
 
@@ -127,12 +122,10 @@ async function checkLabelGuards(
       continue;
     }
 
-    if (userLang && userLang.percentage < tierValue) {
-      const logMessage = logger.error(`${user} does not meet the required tier for ${labelFilter.charAt(0).toUpperCase() + labelFilter.slice(1)}`);
-      hasPassed = false;
-      msg.push(`\n! ${logMessage?.logMessage.raw}`);
-    } else if (!userLang) {
-      const logMessage = logger.error(`${user} does not have the required language for: ${labelFilter.charAt(0).toUpperCase() + labelFilter.slice(1)}`);
+    if (userLangPercentage < tierValue) {
+      const logMessage = logger.error(
+        `${user} does not meet the required tier for ${labelFilter.charAt(0).toUpperCase() + labelFilter.slice(1)}`
+      );
       hasPassed = false;
       msg.push(`\n! ${logMessage?.logMessage.raw}`);
     }
